3023a6f1c93290ff0358cc14ba5a06cdd0597ecf
julesnuggy <julesnuggy@gmail.com>
2020-01-16 23:29:10 +0000

    Add AboutMe section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faMapMarkedAlt, faLaptopCode, faAddressCard } from '@fortawesome/free-solid-svg-icons'
 
+import AboutMe from './components/aboutMe';
 import Header from './components/header';
 import Experience from './components/experience';
 import data from './data/experienceData.json';
@@ -13,6 +14,7 @@ const App: React.FC = () => {
   return (
     <div className="app-body">
       <Header />
+      <AboutMe />
       <div className="experience-container">
         {data.map((d, idx) => <Experience data={d} key={idx} />)}
       </div>
