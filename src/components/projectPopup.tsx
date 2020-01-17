dd37c7f82decfc8dadeec6cdfd93968699a4b2b5
julesnuggy <julesnuggy@gmail.com>
2020-01-17 21:54:25 +0000

    Update ProjectPopup styles

--- src/components/projectPopup.tsx
+++ src/components/projectPopup.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 
 import { ProjectData } from '../data/experienceData';
 import '../styles/project.css'
+import '../App.css'
 
 type ProjectProps = {
   data: ProjectData;
@@ -10,19 +11,48 @@ type ProjectProps = {
   onClick: (index: number) => void;
 }
 
-const ProjectPopup = ({data, index, onClick}: ProjectProps) => (
-  <div className="project-popup" onClick={() => onClick(index)}>
-    <div className="project-details">
-      <div className="core-information">
-        <div>{data.name}</div>
-        <div>{data.role}</div>
-        <div>{data.from} - {data.to}</div>
-      </div>
-    <div>Tech Stack:</div>
+type DataProps = {
+  data: ProjectData;
+}
+
+const CoreInformation = ({data}: DataProps) => (
+  <div className="core-information">
+    <div className="tag">{`<BasicInfo>`}</div>
+    <div className="indented-text">
+      <div>{data.name}</div>
+      <div>{data.role}</div>
+      <div>{data.from} - {data.to}</div>
+    </div>
+    <div className="tag">{`</BasicInfo>`}</div>
+  </div>
+);
+
+const TechStack = ({data}: DataProps) => (
+  <div className="tech-stack-container">
+    <div className="tag">{`<TechStack>`}</div>
     <div className="tech-stack-list">
       {data.techStack.map((t, idx) => <li className="tech-stack-item" key={idx}>{t}</li>)}
     </div>
-      <ReactMarkdown>{data.description}</ReactMarkdown>
+    <div className="tag">{`</TechStack>`}</div>
+  </div>
+);
+
+const Description = ({data}: DataProps) => (
+  <>
+    <div className="tag">{`<Description>`}</div>
+    <ReactMarkdown className="description indented-text">
+      {data.description}
+    </ReactMarkdown>
+    <div className="tag">{`</Description>`}</div>
+  </>
+);
+
+const ProjectPopup = ({data, index, onClick}: ProjectProps) => (
+  <div className="project-popup code-block dark-theme" onClick={() => onClick(index)}>
+    <div className="project-details">
+      <CoreInformation data={data} />
+      <TechStack data={data} />
+      <Description data={data} />
     </div>
   </div>
 );
