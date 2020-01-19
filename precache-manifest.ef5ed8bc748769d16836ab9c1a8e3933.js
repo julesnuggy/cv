df8bde7ebd58b6e66c3eaf226d0b1adc2594c0c5
julesnuggy <julesnuggy@gmail.com>
2020-01-19 20:39:35 +0000

    Updates

---
+++ precache-manifest.ef5ed8bc748769d16836ab9c1a8e3933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a64b7cbb899a12879f499821169f6f4",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "66824a0ec3cbb8c59a54",
+    "url": "/cv/static/css/main.c7b6d462.chunk.css"
+  },
+  {
+    "revision": "ed280324ec480348cb6e",
+    "url": "/cv/static/js/2.e95f0aca.chunk.js"
+  },
+  {
+    "revision": "76d7a8da27b1246d0ec7305dafc313ce",
+    "url": "/cv/static/js/2.e95f0aca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66824a0ec3cbb8c59a54",
+    "url": "/cv/static/js/main.879dff81.chunk.js"
+  },
+  {
+    "revision": "dbf1a27ccd4839709517",
+    "url": "/cv/static/js/runtime-main.e8f205ea.js"
+  },
+  {
+    "revision": "33b9d55802b2fd00a33e522f1849151f",
+    "url": "/cv/static/media/profile_photo.33b9d558.jpg"
+  }
+]);
