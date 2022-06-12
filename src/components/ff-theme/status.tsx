1d1f73c95af93c653f6139cb32823ebe8c8cbce3
Julian Ng <julesnuggy@gmail.com>
2022-06-12 14:01:48 +0100

    Style stat values positioning

--- src/components/ff-theme/status.tsx
+++ src/components/ff-theme/status.tsx
@@ -1,27 +1,42 @@
 import React from 'react'
-import Card from './card';
 
 import Section from './section';
 import profile_photo from '../../assets/profile_photo.jpg';
 import '../../styles/ff-theme/status.scss';
-
 
 const Status = () => {
   return (
       <Section title="Status">
         <div className='status-container'>
           <div className="character-name">Julian Ng</div>
-          <p><span className="stats-title">He / Him</span></p>
+          <p><span className="stat-title">He / Him</span></p>
           <img className="character-avatar" src={profile_photo} alt="Julian Ng"/>
 
           <div className="stats">
-            <p><span className="stats-title">LV</span> <span>99</span></p>
-            <p><span className="stats-title">HP</span> <span>9999/9999</span></p>
-            <p><span className="stats-title">MP</span> <span>999/999</span></p>
+            <StatContainer title="LV" current={86} />
+            <StatContainer title="HP" current={7777} max={7777} />
+            <StatContainer title="MP" current={888} max={888} />
           </div>
         </div>
       </Section>
   )
 }
+
+type StatContainerProps = {
+  title: string;
+  current: number;
+  max?: number;
+}
+
+const StatContainer = ({ title, current, max }: StatContainerProps) => (
+  <p>
+    <div className="stat-value-container">
+      <span className="stat-title">{title}</span>
+      <span className="stat-current">{current}</span>
+      {max && <span className="stat-divider">/</span>}
+      {max && <span className="stat-max">{max}</span>}
+    </div>
+  </p>
+)
 
 export default Status;
