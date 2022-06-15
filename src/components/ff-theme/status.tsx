737b3792fc02fdff59d2c6851c0b0c809fa04395
Julian Ng <julesnuggy@gmail.com>
2022-06-15 17:26:58 +0100

    Implement SectionBasic for Status

--- src/components/ff-theme/status.tsx
+++ src/components/ff-theme/status.tsx
@@ -1,42 +1,35 @@
 import React from 'react'
 
-import Section from './section';
+import SectionBasic from './sectionBasic';
 import profile_photo from '../../assets/profile_photo.jpg';
-import '../../styles/ff-theme/status.scss';
 
 const Status = () => {
   return (
-      <Section title="Status">
-        <div className='status-container'>
-          <div className="character-name">Julian Ng</div>
-          <p><span className="stat-title">He / Him</span></p>
-          <img className="character-avatar" src={profile_photo} alt="Julian Ng"/>
-
-          <div className="stats">
-            <StatContainer title="LV" current={86} />
-            <StatContainer title="HP" current={7777} max={7777} />
-            <StatContainer title="MP" current={888} max={888} />
-          </div>
-        </div>
-      </Section>
+    <>
+      <SectionBasic
+        section="Status"
+        title="Julian Ng"
+        subtitle="He / Him"
+        avatar_src={profile_photo}
+        stats={[
+          {
+            title: 'LV',
+            current: 86
+          },
+          {
+            title: 'HP',
+            current: 7777,
+            max: 7777
+          },
+          {
+            title: 'MP',
+            current: 888,
+            max: 888,
+          }
+        ]}
+      />
+    </>
   )
 }
-
-type StatContainerProps = {
-  title: string;
-  current: number;
-  max?: number;
-}
-
-const StatContainer = ({ title, current, max }: StatContainerProps) => (
-  <p>
-    <span className="stat-value-container">
-      <span className="stat-title">{title}</span>
-      <span className="stat-current">{current}</span>
-      {max && <span className="stat-divider">/</span>}
-      {max && <span className="stat-max">{max}</span>}
-    </span>
-  </p>
-)
 
 export default Status;
