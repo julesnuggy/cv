281487f59f9fe02b32cb94fdf91a550e69c2a246
Julian Ng <julesnuggy@gmail.com>
2022-07-26 15:40:31 +0100

    Enable menu item hover selection

--- src/components/ff-theme/menu.tsx
+++ src/components/ff-theme/menu.tsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import Card from './card';
 
+import pointer_animated from '../../assets/pointer_animated.gif';
 import '../../styles/ff-theme/menu.scss'
 
 type MenuProps = {
   items: string[]
 }
 
+type MenuItemProps = {
+  item: string;
+  isFocused: boolean;
+  onFocus: () => void;
+}
+
+const MenuItem = ({ item, isFocused, onFocus, }: MenuItemProps) => (
+  <div className='menu-item-container'>
+    <div className='pointer-container'>
+      {isFocused && (<img src={pointer_animated} alt='-->' />)}
+    </div>
+    <button onFocus={onFocus} onPointerOver={onFocus}>
+      <div className='item-text'>{item}</div>
+    </button>
+  </div>
+);
+
+
 const Menu = ({ items }: MenuProps) => {
+  const [ focusIndex, setFocusIndex ] = useState(0);
+
+  const onFocus = (index: number) => setFocusIndex(index);
+
   return (
     <div className='menu-container'>
       <Card>
-        {items.map(item => (
-          <div className='menu-item'>{item}</div>
+        {items.map((item, idx) => (
+          <MenuItem item={item} isFocused={focusIndex === idx} onFocus={() => onFocus(idx)} />
         ))}
       </Card>
     </div>
