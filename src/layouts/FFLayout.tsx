1f6630554184d8ed45d8aea6e2f11e32751e992f
Julian Ng <julesnuggy@gmail.com>
2022-07-27 11:08:59 +0100

    Adjust FFLayout to menu-content style

--- src/layouts/FFLayout.tsx
+++ src/layouts/FFLayout.tsx
@@ -4,13 +4,19 @@ import History from '../components/ff-theme/History';
 import Menu from '../components/ff-theme/Menu';
 import Status from '../components/ff-theme/Status';
 
+import '../styles/layouts/FFLayout.scss';
+
 const FFLayout = () => {
   return (
-    <>
-      <Menu items={['Status', 'Abilities', 'Lore']} />
-      <Status />
-      <History />
-    </>
+    <div className="ff-layout">
+      <div className="ff-layout-menu">
+        <Menu items={['Status', 'Abilities', 'Lore']} />
+      </div>
+      <div className="ff-layout-content">
+        <Status />
+        <History />
+      </div>
+    </div>
   )
 }
 
