4fe26979e65f8a990ac98d23c69472edbbdbb490
Julian Ng <julesnuggy@gmail.com>
2022-08-10 11:19:44 +0100

    Refactor projects in Experience component

--- src/components/coding-theme/Experience.tsx
+++ src/components/coding-theme/Experience.tsx
@@ -8,11 +8,42 @@ import { ExperienceData } from '../../data/experienceData';
 import '../../styles/coding-theme/experience.scss';
 import '../../App.scss';
 
-type EmployerCardProps = {
+type ExperienceProjectsProps = {
+  data: ExperienceData;
+  expandedProjects: number[];
+  handleProjectClick: (index: number) => void;
+  handleClosePopup: () => void
+}
+
+type ExperienceProps = {
   data: ExperienceData;
 };
 
-const Experience = ({data}: EmployerCardProps) => {
+const ExperienceProjects = ({ data, expandedProjects, handleProjectClick, handleClosePopup }: ExperienceProjectsProps) => {
+  return (
+    <div className="projects-container">
+      {data.projects.map((p, idx) =>
+        <div className="project-container" key={`${data.employer}-${p.name}-${idx}`}>
+          <Project data={p} index={idx} onClick={handleProjectClick} />
+          <CSSTransition timeout={200} in={expandedProjects.includes(idx)} classNames="project-popup" key={idx}>
+            <>
+              {
+                expandedProjects.includes(idx) &&
+                  <ProjectPopup
+                      data={p}
+                      key={`pp-${idx}`}
+                      closePopup={handleClosePopup}
+                  />
+              }
+            </>
+          </CSSTransition>
+        </div>
+      )}
+    </div>
+  )
+}
+
+const Experience = ({data}: ExperienceProps) => {
   const [ expandedProjects, setExpandedProjects ] = useState([-1]);
 
   const handleProjectClick = (index: number) => {
@@ -34,25 +65,12 @@ const Experience = ({data}: EmployerCardProps) => {
         {data.employer} || {data.from} - {data.to} || {data.location}
       </div>
       <ReactMarkdown className="code-block" source={data.summary} />
-      <div className="projects-container">
-        {data.projects.map((p, idx) =>
-          <div className="project-container" key={`${data.employer}-${p.name}-${idx}`}>
-            <Project data={p} index={idx} onClick={handleProjectClick} />
-            <CSSTransition timeout={200} in={expandedProjects.includes(idx)} classNames="project-popup" key={idx}>
-              <>
-                {
-                  expandedProjects.includes(idx) &&
-                  <ProjectPopup
-                    data={p}
-                    key={`pp-${idx}`}
-                    closePopup={handleClosePopup}
-                  />
-                }
-              </>
-            </CSSTransition>
-          </div>
-        )}
-      </div>
+      <ExperienceProjects
+        data={data}
+        expandedProjects={expandedProjects}
+        handleProjectClick={handleProjectClick}
+        handleClosePopup={handleClosePopup}
+      />
     </div>
   );
 };
