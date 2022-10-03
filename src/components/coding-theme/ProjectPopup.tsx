7b1b1b982b2dc631354b144f898881397a22e8b0
Julian Ng <julesnuggy@gmail.com>
2022-10-03 11:35:50 +0100

    Update project popup UI

--- src/components/coding-theme/ProjectPopup.tsx
+++ src/components/coding-theme/ProjectPopup.tsx
@@ -25,35 +25,35 @@ const handleEscapePress = (event: KeyboardEvent, onEscapePress: () => void) => {
 };
 
 const CoreInformation = ({data}: DataProps) => (
-  <>
+  <div className="indented-tag">
     <div className="tag">{'<BasicInfo>'}</div>
     <div className="indented-text">
-      <div>{data.name}</div>
-      <div>{data.role}</div>
-      <div>{data.from} - {data.to}</div>
+      <div>Client: {data.name}</div>
+      <div>Role: {data.role}</div>
+      <div>Dates: {data.from} - {data.to}</div>
     </div>
     <div className="tag">{'</BasicInfo>'}</div>
-  </>
+  </div>
 );
 
 const TechStack = ({data}: DataProps) => (
-    <>
+    <div className="indented-tag">
       <div className="tag">{'<TechStack>'}</div>
       <div className="tech-stack-list">
         {data.techStack.map((t, idx) => <li className="tech-stack-item" key={idx}>{t}</li>)}
       </div>
       <div className="tag">{'</TechStack>'}</div>
-    </>
+    </div>
 );
 
 const Description = ({data}: DataProps) => (
-  <>
+  <div className="indented-tag">
     <div className="tag">{'<Description>'}</div>
     <ReactMarkdown className="description indented-text" renderers={{link: LinkRenderer}}>
       {data.description}
     </ReactMarkdown>
     <div className="tag">{'</Description>'}</div>
-  </>
+  </div>
 );
 
 const ProjectPopup = ({data, closePopup}: ProjectProps) => {
@@ -71,9 +71,11 @@ const ProjectPopup = ({data, closePopup}: ProjectProps) => {
         <div className="project-popup-close" onClick={closePopup}>
           <FontAwesomeIcon icon={['far', 'times-circle']} size="lg" />
         </div>
+        <div className="tag">{'<Project>'}</div>
         <CoreInformation data={data} />
         <TechStack data={data} />
         <Description data={data} />
+        <div className="tag">{'</Project>'}</div>
       </div>
     </div>
   </>
